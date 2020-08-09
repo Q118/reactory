fc9d043d56ca2bf2734431ab7dbc0e7b9190f46d
Q118 <shelbyfish91@gmail.com>
2020-08-08 19:24:21 -0700

    buttons styled

--- src/components/PlantTable.js
+++ src/components/PlantTable.js
@@ -1,79 +1,72 @@
+/** @format */
+
 import React from "react";
 import plants from "../plants.json";
 import "bootstrap/dist/css/bootstrap.css";
 import "./style.css";
-import Buttons from "./buttons"
+import Buttons from "./buttons";
 
 class PlantTable extends React.Component {
-    state = { order: "ascending" };
-  
+	state = { order: "ascending" };
 
-    // function to soprt by Name
-    handleNameSort = () => {
-      this.setState({
-        order: this.state.order === "ascending" ? "descending" : "ascending",
-      });
-    };
-    render() {
-      const sortedPlants = plants.sort((a, b) => {
-        if (a.Name === b.Name) {
-          return 0;
-        }
-        if (this.state.order === "ascending") {
-          if (a.Name < b.Name) {
-            return -1;
-          }
-          return 1;
-        }
-        if (a.Name < b.Name) {
-          return 1;
-        }
-        return -1;
-      });
-      return (
-        <div className = "container mt-4">
-          <header className= "headerContainer mb-4 pt-2 text-center">
-            <h1>Medicinal Plants</h1>
-          </header>
-          <Buttons 
-          handleNameSort={this.handleNameSort}
-          
-          />
-          
-          
-    
-          <table className="table tableContainer">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">
-                  Name
-                </th>
-                <th scope="col">Benefits</th>
-                <th scope="col">Safety</th>
-                <th scope="col">Picture</th>
-              </tr>
-            </thead>
-            <tbody>
-              {sortedPlants.map((plant) => (
-                <tr key={plant.Id}>
-                  <th scope="row">{plant.Id}</th>
-                  <td>{plant.Name}</td>
-                  <td>{plant.Benefits}</td>
-                  <td>{plant.Safety}</td>
-                  <td> <img 
-                  alt = {plant.Name}
-                  src = {plant.Image} 
-                  className= "picture" 
-                  /> 
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      );
-    }
-  }
-  
-  export default PlantTable;
+	// function to soprt by Name
+	handleNameSort = () => {
+		this.setState({
+			order: this.state.order === "ascending" ? "descending" : "ascending",
+		});
+	};
+	render() {
+		const sortedPlants = plants.sort((a, b) => {
+			if (a.Name === b.Name) {
+				return 0;
+			}
+			if (this.state.order === "ascending") {
+				if (a.Name < b.Name) {
+					return -1;
+				}
+				return 1;
+			}
+			if (a.Name < b.Name) {
+				return 1;
+			}
+			return -1;
+		});
+		return (
+			<div className="container mt-4">
+				<header className="headerContainer mb-2 pt-2 text-center">
+					<h1>Medicinal Plants</h1>
+				</header>
+        <Buttons
+         handleNameSort={this.handleNameSort} 
+         />
+				<table className="table tableContainer">
+					<thead>
+						<tr>
+							<th scope="col">ID</th>
+							<th scope="col">Name</th>
+							<th scope="col">Benefits</th>
+							<th scope="col">Safety</th>
+							<th scope="col">Picture</th>
+						</tr>
+					</thead>
+					<tbody>
+						{sortedPlants.map((plant) => (
+							<tr key={plant.Id}>
+								<th scope="row">{plant.Id}</th>
+								<td><strong>{plant.Name}</strong></td>
+								<td>{plant.Benefits}</td>
+								<td>{plant.Safety}</td>
+								<td>
+									{" "}
+									<img alt={plant.Name} src={plant.Image} className="picture" />
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+}
+
+export default PlantTable;
--- src/components/buttons.js
+++ src/components/buttons.js
@@ -7,31 +7,18 @@ import "./style.css";
 
 function Buttons(props) {
 	return (
-        <div className = "container buttonContainer">
-		<div className="btn-group" role="group" aria-label="Basic example">
-			<button className="btn btn-secondary" onClick={props.handleNameSort}>
-				Left
-			</button>
-			<button className="btn btn-secondary">Right</button>
+		<div className="container buttonContainer mb-2 text-center">
+			<div className="btn-group" role="group" aria-label="Basic example">
+				<button className="btn nameButton btn-lg" onClick={props.handleNameSort}>
+					Sort By Name
+				</button> 
+                &nbsp;&nbsp;&nbsp;
+				<button className="btn safeButton btn-lg">Filter: Safety Above 4</button>
+			</div>
 		</div>
-        </div>
 	);
 }
 
 export default Buttons;
 
-// function sort(option) {
-//     if (option === "id") {
-//         const sorted = [...employees];
-//         sorted.sort((a, b) => a.id > b.id ? 1 : -1);
-//         // setSortedById(sorted);
-//         setEmployees(sorted);
-//         setSortOption("id");
-//     } else if (option === "name") {
-//         const sorted = [...employees];
-//         sorted.sort((a, b) => a.name > b.name ? 1 : -1);
-//         // setSortedByName(sorted);
-//         setEmployees(sorted);
-//         setSortOption("name");
-//     }
-//   }
+
